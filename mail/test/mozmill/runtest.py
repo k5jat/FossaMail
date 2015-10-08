cb3279f0b4a16449996c7ef1da966a8e073b4525
wolfbeast <mcwerewolf@gmail.com>
2015-10-08 17:48:09 +0200

    Remove FE telemetry

--- mail/test/mozmill/runtest.py
+++ mail/test/mozmill/runtest.py
@@ -140,8 +140,6 @@ class ThunderTestProfile(mozrunner.ThunderbirdProfile):
         'mailnews.database.global.logging.upstream': True,
         # Do not allow fonts to be upgraded
         'mail.font.windows.version': 2,
-        # No, we don't want to be prompted about Telemetry
-        'toolkit.telemetry.prompted': 999,
         }
 
     # Dummied up local accounts to stop the account wizard
